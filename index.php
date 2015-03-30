3a17c6d98c59731240a9ee585e906c8a7a498829
Rahul sharma <rahul271092@users.noreply.github.com>
2015-03-30 18:25:44 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include "config.php";
+?>
+<html>
+<head>
+</head>
+<body>
+<div class="container">
+<form method="post">
+Username <input type="text" name="fusername"/>
+<br>
+Password <input type="text" name="password"/>
+<br>
+<input type="submit" value="login"/>
+</form>
+</div>
+</body>
+</html>
